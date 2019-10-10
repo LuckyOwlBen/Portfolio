d378ab24e10b514da7e9ace1a5844eb8d1967ff2
ben malmberg <benmalmberg2@gmail.com>
2019-10-10 10:09:18 -0400

    did part 5

--- TourOfHeroes/src/app/components/heroes/heroes.component.ts
+++ TourOfHeroes/src/app/components/heroes/heroes.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { Hero } from'../../models/hero'
-import { HEROES } from'../../models/mock-heroes';
+import { HeroService } from '../../services/hero.service'
 
 @Component({
   selector: 'app-heroes',
@@ -9,18 +8,15 @@ import { HEROES } from'../../models/mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  selectedHero:Hero;
-  onSelect(hero: Hero): void{
-    this.selectedHero = hero;
-  }
-  heroes = HEROES;
-  hero: Hero = {
-    id:1,
-    name:'Windstorm'
-  };
-  constructor() { }
+  heroes: Hero[];
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
+  getHeroes():void{
+    this.heroService.getHeroes()
+      .subscribe(heroes=>this.heroes=heroes);
+  }
 }
